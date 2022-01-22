4d53c12ff57c6675d6ac2eefbf364b9f0c0481d3
Caleb Lindsey <calebalanlindsey@gmail.com>
2022-01-22 14:02:49 -0500

    first commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Caleb Lindsey on 1/21/22.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
